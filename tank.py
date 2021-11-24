42158285a9e22683d5433a0ac9724fc61edb1cc8
liyifei1997 <51017842+liyifei1997@users.noreply.github.com>
2021-11-23 23:34:41 -0600

    Update tank.py
    1. Main logic:
      a. Start the game
      b. End Game

--- tank.py
+++ tank.py
@@ -1,3 +1,17 @@
 import time
 import random
 import pygame
+
+'''
+1. Main logic:
+  a. Start the game
+  b. End Game
+'''
+class TankMaingame():
+  def __init__(self):
+    pass
+  def GameStart(self):
+    pass
+  def Gameover(self):
+    pass
+  
