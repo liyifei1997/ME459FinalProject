644d1288b27310de89592fd0078decf2444baedd
liyifei1997 <51017842+liyifei1997@users.noreply.github.com>
2021-12-12 22:51:27 -0600

    Update class_ourtank.py

--- class_ourtank.py
+++ class_ourtank.py
@@ -3,9 +3,9 @@ class ourtank():
     self.direction = 'U'                     #Up Down Left Right
     self.pictures = {                        #load pictures
       'U':pygame.image.load(''),
-      'D':,
-      'L':,
-      'R':,
+      'D':pygame.image.load(''),
+      'L':pygame.image.load(''),
+      'R':pygame.image.load(''),
       
     }
     self.picture = self.pictures[self.direction]
