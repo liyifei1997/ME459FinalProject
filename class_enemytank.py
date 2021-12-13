2adf4d016da226aa08f4c72e2cd494f641bfbaf9
liyifei1997 <51017842+liyifei1997@users.noreply.github.com>
2021-12-13 12:32:32 -0600

    Create class_enemytank.py

---
+++ class_enemytank.py
@@ -0,0 +1,12 @@
+import time
+import random
+import pygame
+
+class enemytank(tank):
+  def__init__(self,left,top,speed):
+    self.pictures = {
+      'U': pygame.image.load('')'
+      'D': pygame.image.load('')'
+      'L': pygame.image.load('')'
+      'R': pygame.image.load('')'
+    }
