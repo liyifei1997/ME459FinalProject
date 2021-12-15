e448a724217cb2f3fe7177c5e437645656109d8a
liyifei1997 <51017842+liyifei1997@users.noreply.github.com>
2021-12-15 10:30:41 -0600

    Update class_ammo.py

--- class_ammo.py
+++ class_ammo.py
@@ -1,1 +1,24 @@
+import time
+import random
+import pygame
 
+class ammo():
+  def __init__(self,tank):
+    self.picture = pygame.image.load('')
+    self.direction = tank.direction
+    if self.direction == 'U'
+      self.rect.left = 
+      self.rect.top =
+    elif self.direction == 'D'
+      self.rect.left = 
+      self.rect.top =
+    elif self.direction == 'L'
+      self.rect.left = 
+      self.rect.top =
+    elif self.direction == 'R'
+      self.rect.left = 
+      self.rect.top =
+    self.rect = self.image.get_rect()
+    self.speed = 8
+  def move(self):
+  def displayammo(self):
