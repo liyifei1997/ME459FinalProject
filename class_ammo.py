c5549dd9e38547f8e114ed22bb8e65e43384a572
liyifei1997 <51017842+liyifei1997@users.noreply.github.com>
2021-12-15 10:42:06 -0600

    Update class_ammo.py

--- class_ammo.py
+++ class_ammo.py
@@ -7,17 +7,17 @@ class ammo():
     self.picture = pygame.image.load('')
     self.direction = tank.direction
     if self.direction == 'U'
-      self.rect.left = 
-      self.rect.top =
+      self.rect.left = tank.rect.left + tank.rect.width/2 - self.rect.width/2
+      self.rect.top = tank.rect.top - self.rect.top
     elif self.direction == 'D'
-      self.rect.left = 
-      self.rect.top =
+      self.rect.left = tank.rect.left + tank.rect.width/2 - self.rect.width/2
+      self.rect.top = tank.rect.top + tank.rect.height
     elif self.direction == 'L'
-      self.rect.left = 
-      self.rect.top =
+      self.rect.left = tank.rect.left - self.rect.width/2 - self.rect.width/2
+      self.rect.top = tank.rect.top + tank.rect.width/2 - self.rect.width/2
     elif self.direction == 'R'
-      self.rect.left = 
-      self.rect.top =
+      self.rect.left = tank.rect.left + tank.rect.width
+      self.rect.top = tank.rect.top + tank.rect.width/2 - self.rect.width/2
     self.rect = self.image.get_rect()
     self.speed = 8
   def move(self):
