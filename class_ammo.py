e6d745603a3aac6b268583d3669a92a8955076a2
liyifei1997 <51017842+liyifei1997@users.noreply.github.com>
2021-12-16 17:21:14 -0600

    Update class_ammo.py

--- class_ammo.py
+++ class_ammo.py
@@ -4,6 +4,7 @@ import pygame
 
 class ammo():
   def __init__(self,tank):
+    self.live = True
     self.picture = pygame.image.load('')
     self.direction = tank.direction
     if self.direction == 'U'
@@ -20,6 +21,26 @@ class ammo():
       self.rect.top = tank.rect.top + tank.rect.width/2 - self.rect.width/2
     self.rect = self.image.get_rect()
     self.speed = 8
-  def move(self):
+  def ammomove(self):
+    if self.direction == 'U':
+      if self.rect.top > 0:
+        self.rect.top -= self.speed
+      else:
+        self.live = False
+    elif self.direction == 'D':
+      if self.rect.top < Maingame.SCREEN_HEIGHT - self.rect.height:
+        self.rect.top += self.speed
+      else:
+        self.live = False
+    elif self.direction == 'L':
+      if self.rect.left > 0:
+        self.rect.left -= self.speed
+      else:
+        self.live = False
+    elif self.direction == 'R':
+      if self.rect.left < Maingame.SCREEN_WIDTH - self.rect.width:
+        self.rect.left += self.speed
+      else:
+        self.live = False
   def displayammo(self):
     MainGame.window.blit(self.image,self.rect)
